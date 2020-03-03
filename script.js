3eda275fe886aed2d806ba465a21145b784bdaf6
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2020-03-03 14:49:08 +0200

    Update and rename js/index.js to script.js

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const secondsHand = document.querySelector('.second-hand'),
   minutesHand = document.querySelector('.minute-hand'),
   hourHand = document.querySelector('.hour-hand');
 
+//creating fn to make sure clock updates the seconds/minutes/hour
 function setDate() {
-  const now = new Date();
+  const now = new Date(); //grab the date
 
+  //seconds
   const seconds = now.getSeconds(),
     secondsDeg = (seconds / 60) * 360 + 90;
   secondsHand.style.transform = `rotate(${secondsDeg}deg)`;
   secondsHand.style.transform = `rotate(${secondsDeg}deg)`;
 
+  //minutes
   const minutes = now.getMinutes(),
     minutesDeg = (minutes / 60) * 360 + 90;
   minutesHand.style.transform = `rotate(${minutesDeg}deg)`;
 
+  //hours
   const hours = now.getHours(),
     hoursDeg = (hours / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hoursDeg}deg)`;
@@ -23,4 +27,4 @@ function setDate() {
   }
 }
 
-setInterval(setDate, 1000);
+setInterval(setDate, 1000); //making sure it runs every second
